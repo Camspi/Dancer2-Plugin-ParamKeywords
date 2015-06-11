6b02b6e08ad5f9e4c2b08b7bc96c8ec0c757de37
Chris Tijerina <amarus18@hotmail.com>
2015-06-11 15:15:38 -0700

    Fix repo name, pod

--- lib/Dancer2/Plugin/ParamKeywords.pm
+++ lib/Dancer2/Plugin/ParamKeywords.pm
@@ -1,7 +1,7 @@
 use strict;
 use warnings;
 
-package Dancer2::Plugin::ParamShortcuts;
+package Dancer2::Plugin::ParamKeywords;
 use Dancer2::Plugin;
 
 our $VERSION = 'v0.0.1';
@@ -21,7 +21,7 @@ foreach my $source ( qw( route query body ) ) {
 register munged_params => sub {
      my $dsl = shift;
      my $conf = plugin_setting->{munge_precedence};
-     die 'Please configure the plugin settings for ParamShortcuts to use this keyword'
+     die 'Please configure the plugin settings for ParamKeywords to use this keyword'
        unless ref($conf) eq 'ARRAY';
 
      my %params = map { $dsl->app->params($_) } reverse @$conf;
@@ -33,9 +33,9 @@ register_plugin for_versions => [ 2 ] ;
 1;
 
 # ABSTRACT: Sugar for the params() keyword
-# PODNAME: Dancer2::Plugin::ParamShortcuts
+# PODNAME: Dancer2::Plugin::ParamKeywords
 
-=h1 SYNOPSIS
+=head1 SYNOPSIS
 
     use Dancer2;
     use Dancer2::Plugin::ParamKeywords;
@@ -46,7 +46,7 @@ register_plugin for_versions => [ 2 ] ;
         my $post_param  = body_param('some_named_parameter');
     };
 
-=h1 DESCRIPTION
+=head1 DESCRIPTION
 
 The default L<Dancer2::Core::Request params
 accessor|Dancer2::Core::Request/"params($source)">
@@ -69,7 +69,7 @@ C<$person_id> will equal 2 while still matching the route C</people/1>.
 This plugin provides keywords that wrap around C<params($source)>
 for convenience to fetch parameter values from specific sources.
 
-=h2 CONFIGURATION
+=head2 CONFIGURATION
 
 The C<munged_params> keyword requires you to configure an order of
 precedence by which to prefer parameter sources.  Please see 
@@ -78,7 +78,7 @@ for a list of valid sources.
 
     # In config.yml
     plugins:
-      ParamShortcuts:
+      ParamKeywords:
         munge_precedence:
           - route
           - body
@@ -87,34 +87,34 @@ for a list of valid sources.
 If you won't be using this keyword, you don't need to bother configuring
 this plugin.
 
-=h1 KEYWORDS
+=head1 KEYWORDS
 
-=h2 munged_params
+=head2 munged_params
 
 Returns a hash in list context or a hash reference in scalar context of
 parameters munged according to the precedence provided in the configuration
 file (from highest to lowest).
 
-=h2 query_param(Str)
+=head2 query_param(Str)
 
 Returns the value supplied for a given parameter in the query string.
 
-=h2 query_params(Str)
+=head2 query_params(Str)
 
 Returns the arguments and values supplied by query string. Returns a hash in list context or a hasref in scalar context.
 
-=h2 body_param(Str)
+=head2 body_param(Str)
 
 Returns the value supplied for a given parameter in the C<POST> arguments.
 
-=h2 body_params(Str)
+=head2 body_params(Str)
 
 Returns arguments and values supplied by a C<POST> request.  Returns a hash in list context or a hasref in scalar context.
 
-=h2 route_param(Str)
+=head2 route_param(Str)
 
 Returns the value supplied for a given named parameter in the route.
 
-=h2 route_params(Str)
+=head2 route_params(Str)
 
 Returns the arguments and values suppled by the route. Returns a hash in list context or a hasref in scalar context.
